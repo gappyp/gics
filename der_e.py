79202ef564ad8101e63adf140b08ac4a91948d7e
gappyp <gp8899@gmail.com>
2018-07-19 11:49:56 +1000

    get basic version going

--- der_e.py
+++ der_e.py
@@ -1,5 +1,5 @@
 """
-* TODO: thing in argparse that lets specify valid options. will be good for e.g. window functions
+* TODO: thing in argparse that lets specify valid options
 """
 
 import argparse
@@ -16,30 +16,22 @@ from geomag.kdu_viewer.help import AttrDict
 
 pd.options.display.max_rows = 1000
 
-init_fn = Path(r'G:\python_projs\gics\.der_e')      # TODO: change so can use different. still like the @thing... maybe use and fix it eventually
+init_fn = Path(r'G:\python_projs\gics\.der_e')      # TODO: option to use different file
 with init_fn.open('r') as fp:
     init_args = fp.read()
 
 parser = argparse.ArgumentParser(description='Derive electric fields')
-
-parser.add_argument('-f', action='append',
-                    dest='fns',
-                    type=str,
-                    default=[],
-                    help='time series file to include in processing')
-parser.add_argument('--filelist', action='append',              # TODO: don't know if need this... might be better to get the recursive '@' cmd options working
-                    dest='filelists',
-                    default=[],
-                    help='filename of a list of time series files to include in processing')
+parser.add_argument('-f', action='append', dest='fns', type=str, default=[], help='time series file to include in processing')
 parser.add_argument('-a', action="store", dest="hs_alpha", type=float, help='half-space alpha value')      # TODO: can put ranges on this e.g. choices=[Range(0.0, 1.0)]. see https://stackoverflow.com/questions/12116685/how-can-i-require-my-python-scripts-argument-to-be-a-float-between-0-0-1-0-usin
 parser.add_argument('-i', action="store", dest="it_fn", type=Path, help='impedance tensor filename')      # TODO: can put ranges on this e.g. choices=[Range(0.0, 1.0)]. see https://stackoverflow.com/questions/12116685/how-can-i-require-my-python-scripts-argument-to-be-a-float-between-0-0-1-0-usin
 parser.add_argument('-o', action="store", dest="out_fn", type=Path, help='output filename (if not specified will be outputted to stdout)')
-parser.add_argument('-w', action="store", dest="wnd_funct", type=str, help='windowing function (if not specified will be outputted to stdout)')
 parser.add_argument('--max-ts-gap', action="store", dest="max_ts_gap", type=int, help='maximum number of missing samples that will interpolate')
 parser.add_argument('--ts-gap-interp-method', action="store", dest="ts_gap_interp_method", type=str, help='interpolation method to use for time series gaps')       # TODO: these can't be optional
 parser.add_argument('--lb', action="store", dest="lb", type=float, help='lower bound for bandpass filter (log scale)')       # TODO: these can't be optional
 parser.add_argument('--ub', action="store", dest="ub", type=float, help='upper bound for bandpass filter (log scale)')       # TODO: these can't be optional
 # TODO: zero pad to a certain number of days? or a certain number of samples?
+parser.add_argument('-c', action="store", dest="col_order", type=str, help='order of columns')
+parser.add_argument('--figs', action="store_true", dest="show_figs", help='show figures')
 
 init_args = shlex.split(init_args, comments=True)
 
@@ -60,10 +52,15 @@ for fn in args.fns:
         temp.append(single_file)
 args.fns = temp
 
-#pprint(args)
 # ======================================================================================================================
 # parse impedance tensor
 Z, (Z_ll, Z_ul) = parse_edi(args.it_fn)
+
+# fix col order
+if args.col_order != None:
+    args.col_order = [int(x) for x in args.col_order.split(',')]
+else:
+    args.col_order = [6,7,11,12]
 
 # ======================================================================================================================
 # from https://stackoverflow.com/questions/12269528/using-python-pandas-to-parse-csv-with-date-in-format-year-day-hour-min-sec
@@ -71,7 +68,7 @@ def dt_parser(Y, m, d, H, M, S):
     return np.datetime64('{}-{}-{}T{}:{}:{}'.format(Y, m, d, H, M, S))      # TODO: figure out if np.datetime doesn't need to come from string... maybe a tuple?
 
 # read in the data
-# TODO: pandas merge for now. optimize if too slow
+# TODO: pandas merge for now. think this is slowing down when concatenating a lot of files
 dfs = []
 for fn in args.fns:
     df = pd.read_csv(fn, delim_whitespace=True, header=None, parse_dates={'datetime': list(range(6))},
@@ -79,7 +76,8 @@ for fn in args.fns:
                      comment='#'
                      )
     # change names
-    df = df.rename(columns={6:'Bx', 7:'By', 11:'Ex', 12:'Ey'})
+    #df = df.rename(columns={6:'Bx', 7:'By', 11:'Ex', 12:'Ey'})
+    df = df.rename(columns=dict(zip(args.col_order, ['Bx', 'By', 'Ex', 'Ey'])))
     dfs.append(df)
 
 df = pd.concat(dfs, axis='rows')
@@ -87,16 +85,21 @@ df.sort_index(inplace=True)
 df = df.resample('S').mean()
 df.interpolate(limit=3, inplace=True)       # TODO: replace with missing sample limit
 
+# need to replace with 0.0
+df.fillna(0.0, inplace=True)
+
+print(args.show_figs)
+
+if args.show_figs:
+    df.plot(subplots=True)
+    plt.figure()
+
 # ======================================================================================================================
-if args.wnd_funct != None:
-    exec('wnd_funct = np.{}(len(df.index))'.format(args.wnd_funct))
-else:
-    wnd_funct = np.ones(len(df.index))
 
 # take dfts
 dfts = AttrDict()
 for col in df.columns:
-    dfts[col] = np.fft.rfft(wnd_funct*df[col].data)
+    dfts[col] = np.fft.rfft(df[col].data)
 
 f = np.fft.rfftfreq(n=len(df.index))
 
@@ -121,8 +124,7 @@ exp_Ex_f[~mask] = 0.0
 exp_Ex_f *= bpf
 exp_Ex_t = np.fft.irfft(exp_Ex_f)
 
-plt.plot(der_Ex_t)
-plt.plot(exp_Ex_t)
-
-plt.ylim(-20, 20)
-plt.show()
+if args.show_figs:
+    plt.plot(der_Ex_t, alpha=0.7)
+    plt.plot(exp_Ex_t, alpha=0.7)
+    plt.show()
